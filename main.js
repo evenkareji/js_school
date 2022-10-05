8f66ebe967a491be7693ed2bedd4ed8de86c3b29
evenkareji <yokota.tomo222282@gmail.com>
2022-10-05 10:43:36 +0900

    てｓｔ

---
+++ main.js
@@ -0,0 +1,7 @@
+// console.log('j')
+// const body = document.querySelector('body')
+// let body_width = document.documentElement.clientWidth
+
+// let body_height = document.documentElement.clientHeight
+
+// console.log(body_width, body_height)
